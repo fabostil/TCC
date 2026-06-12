f3a00cb33b205c349c1ca678e5fe4fa407e87e32
fabostil <fabinbin7.000@gmail.com>
2026-06-11 23:09:43 -0300

    feat: implementa loop de escuta continua na voice_page

--- app_voz/ios/Flutter/flutter_export_environment.sh
+++ app_voz/ios/Flutter/flutter_export_environment.sh
@@ -2,6 +2,7 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\Acer\develop\flutter"
 export "FLUTTER_APPLICATION_PATH=C:\Users\Acer\TCC\app_voz"
+export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=C:\Users\Acer\TCC\app_voz\ios\Flutter\ephemeral\Packages\.packages\FlutterFramework"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
